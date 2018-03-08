216926f2c3749052566b4ba067d7991b918e3ecf
kbickar <TrumpetGod@gmail.com>
2018-03-07 19:01:28 -0500

    Update from HASS

--- sense.py
+++ sense.py
@@ -1,90 +1,120 @@
 """
 Support for monitoring a Sense energy sensor.
 
-
+For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/sensor.sense/
 """
 import logging
 from datetime import timedelta
 
-import requests.exceptions
 import voluptuous as vol
 
 from homeassistant.components.sensor import PLATFORM_SCHEMA
-from homeassistant.const import (CONF_EMAIL, CONF_PASSWORD)
+from homeassistant.const import (CONF_EMAIL, CONF_PASSWORD,
+                                 CONF_MONITORED_CONDITIONS)
 from homeassistant.helpers.entity import Entity
 from homeassistant.util import Throttle
 import homeassistant.helpers.config_validation as cv
-import homeassistant.util.dt as dt_util
-from sense_api import Senseable
 
-REQUIREMENTS = ['sense-energy==0.1']
+REQUIREMENTS = ['sense_energy==0.3.1']
 
 _LOGGER = logging.getLogger(__name__)
 
-ACTIVE_NAME = "Energy Usage"
-ACTIVE_SOLAR_NAME = "Solar Energy Generation"
-DAILY_NAME = "Daily Energy Usage"
+ACTIVE_NAME = "Energy"
+PRODUCTION_NAME = "Production"
+CONSUMPTION_NAME = "Usage"
 
 ACTIVE_TYPE = 'active'
-ACTIVE_SOLAR_TYPE = 'solar'
-DAILY_TYPE = 'daily'
+
+
+class SensorConfig(object):
+    """Data structure holding sensor config."""
+
+    def __init__(self, name, sensor_type):
+        """Sensor name and type to pass to API."""
+        self.name = name
+        self.sensor_type = sensor_type
+
+
+# Sensor types/ranges
+SENSOR_TYPES = {'active': SensorConfig(ACTIVE_NAME, ACTIVE_TYPE),
+                'daily': SensorConfig('Daily', 'DAY'),
+                'weekly': SensorConfig('Weekly', 'WEEK'),
+                'monthly': SensorConfig('Monthly', 'MONTH'),
+                'yearly': SensorConfig('Yearly', 'YEAR')}
+
+# Production/consumption variants
+SENSOR_VARIANTS = [PRODUCTION_NAME.lower(), CONSUMPTION_NAME.lower()]
+
+# Valid sensors for configuration
+VALID_SENSORS = ['%s_%s' % (typ, var)
+                 for typ in SENSOR_TYPES
+                 for var in SENSOR_VARIANTS]
 
 ICON = 'mdi:flash'
 
-MIN_TIME_BETWEEN_DAILY_UPDATES = timedelta(seconds=150)
-MIN_TIME_BETWEEN_ACTIVE_UPDATES = timedelta(seconds=10)
+MIN_TIME_BETWEEN_DAILY_UPDATES = timedelta(seconds=300)
+MIN_TIME_BETWEEN_ACTIVE_UPDATES = timedelta(seconds=60)
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_EMAIL): cv.string,
     vol.Required(CONF_PASSWORD): cv.string,
+    vol.Required(CONF_MONITORED_CONDITIONS):
+        vol.All(cv.ensure_list, vol.Length(min=1), [vol.In(VALID_SENSORS)]),
 })
 
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Set up the Sense sensor."""
+    from sense_energy import Senseable
+
     username = config.get(CONF_EMAIL)
     password = config.get(CONF_PASSWORD)
 
-    try:
-        data = Senseable(username, password)
-    except:
-        _LOGGER.critical("Connection/Authentication Failure")
-        return
+    data = Senseable(username, password)
 
     @Throttle(MIN_TIME_BETWEEN_DAILY_UPDATES)
-    def update_daily():
+    def update_trends():
         """Update the daily power usage."""
-        data.get_daily_usage()
+        data.update_trend_data()
 
     @Throttle(MIN_TIME_BETWEEN_ACTIVE_UPDATES)
     def update_active():
         """Update the active power usage."""
         data.get_realtime()
 
-    add_devices([
-        # Active power usage
-        Sense(data, ACTIVE_NAME, ACTIVE_TYPE, update_active),
-        # Active power generation
-        Sense(data, ACTIVE_SOLAR_NAME, ACTIVE_SOLAR_TYPE, update_active),
-        # Daily Power
-        Sense(data, DAILY_NAME, DAILY_TYPE, update_daily)])
+    devices = []
+    for sensor in config.get(CONF_MONITORED_CONDITIONS):
+        config_name, prod = sensor.rsplit('_', 1)
+        name = SENSOR_TYPES[config_name].name
+        sensor_type = SENSOR_TYPES[config_name].sensor_type
+        is_production = prod == PRODUCTION_NAME.lower()
+        if sensor_type == ACTIVE_TYPE:
+            update_call = update_active
+        else:
+            update_call = update_trends
+        devices.append(Sense(data, name, sensor_type,
+                             is_production, update_call))
 
+    add_devices(devices)
 
 
 class Sense(Entity):
     """Implementation of a Sense energy sensor."""
 
-    def __init__(self, data, name, sensor_type, update_call):
+    def __init__(self, data, name, sensor_type, is_production, update_call):
         """Initialize the sensor."""
-        self._name = name
+        name_type = PRODUCTION_NAME if is_production else CONSUMPTION_NAME
+        self._name = "%s %s" % (name, name_type)
         self._data = data
         self._sensor_type = sensor_type
         self.update_sensor = update_call
+        self._is_production = is_production
         self._state = None
 
-        if sensor_type == ACTIVE_TYPE or sensor_type == ACTIVE_SOLAR_TYPE:
+        if sensor_type == ACTIVE_TYPE:
             self._unit_of_measurement = 'W'
-        elif sensor_type == DAILY_TYPE:
+        else:
             self._unit_of_measurement = 'kWh'
 
     @property
@@ -112,9 +142,11 @@ class Sense(Entity):
         self.update_sensor()
 
         if self._sensor_type == ACTIVE_TYPE:
-            self._state = round(self._data.active_power)
-        elif self._sensor_type == ACTIVE_SOLAR_TYPE:
-            self._state = round(self._data.active_solar_power)
-        elif self._sensor_type == DAILY_TYPE:
-            self._state = round(self._data.get_daily_usage(),1)
-
+            if self._is_production:
+                self._state = round(self._data.active_solar_power)
+            else:
+                self._state = round(self._data.active_power)
+        else:
+            state = self._data.get_trend(self._sensor_type,
+                                         self._is_production)
+            self._state = round(state, 1)
